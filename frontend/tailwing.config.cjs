24b903e1909f2f4c1ea59d0ea4801e7bcc3dcb95
gaur3009-p <gaur3009@gmail.com>
2025-08-13 20:22:47 +0530

    Create tailwing.config.cjs
    Signed-off-by: gaur3009-p <gaur3009@gmail.com>

---
+++ frontend/tailwing.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6C5CE7',
+        accent: '#FF7675'
+      }
+    }
+  },
+  plugins: []
+}
